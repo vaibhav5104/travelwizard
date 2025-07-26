9e51bec9050028d27546791c40507f0af6b97bde
Vaibhav Sharma <vaibhavsharma5104@gmail.com>
2025-07-27 01:30:15 +0530

    update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,27 +9,26 @@ import {App} from './App'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="982582505218-13jkb29neha7n0stl5ltvao2hin2u0cu.apps.googleusercontent.com">
-    <AuthProvider>
-      <StrictMode>
-        <App />
-        <ToastContainer
-          position="top-right"
-          autoClose={1500}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-          transition={Bounce}
-          bodyClassName="toastBody"
-        />
-      </StrictMode>
-    </AuthProvider>
-  </GoogleOAuthProvider>
-
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+  <AuthProvider>
+    <StrictMode>
+      <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+        bodyClassName="toastBody"
+      />
+    </StrictMode>
+  </AuthProvider>
+</GoogleOAuthProvider>
   
 )
